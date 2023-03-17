609ba0c984acc5d0a8dbe987bfada717b4bd5fdc
raikling <raiklings@outlook.com>
2023-03-17 15:55:42 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,8 +7,8 @@ int main(void)
 {
 int d;
 char low;
-for (d = '0'; d < 10; d++)
-putchar('0' + d);
+for (d = '0'; d < '10'; d++)
+putchar(d);
 for (low = 'a'; low <= 'f'; low++)
 putchar(low);
 putchar('\n');
