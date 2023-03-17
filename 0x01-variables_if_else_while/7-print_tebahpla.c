1043107beb326465ed14b5c12e372aac3a620499
raikling <raiklings@outlook.com>
2023-03-17 15:39:25 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets in reverse
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char low;
+for (low = 'z'; low >= 'a'; low--)
+putchar(low);
+putchar('\n')
+return (0);
+}
