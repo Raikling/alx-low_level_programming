f303f4e8c191d7e0e5a09f78a07d743193f3a49c
raikling <raiklings@outlook.com>
2023-03-17 15:18:53 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print all single digit number of base starting from 0
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+for ( int i = 0; i < 10; i++)
+printf("%d", i);
+return (0);
+}
