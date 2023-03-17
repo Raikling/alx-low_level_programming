6bc664c93872e67c9ea5447f1d0bc399f78e93b7
raikling <raiklings@outlook.com>
2023-03-17 14:59:32 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in lower and upper case
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+char low, upr;
+
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+
+  
+for(upr = 'A'; upr <= 'Z'; upr++)
+putchar(upr);
+putchar('\n');
+return (0);
+}
