7f08b3dbec8cefda3f1f88195dd9bbb04ef897fa
raikling <raiklings@outlook.com>
2023-03-17 15:53:07 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print numbers in base 16
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int d;
+char low;
+for (d = '0'; d < 10; d++)
+putchar(d);
+for (low = 'a'; low <= 'f'; low++)
+putchar(low);
+putchar('\n');
+return (0);
+}
