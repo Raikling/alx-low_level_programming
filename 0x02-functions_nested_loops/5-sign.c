9fb65ff638af762d450724557de96379db8b67a1
raikling <raiklings@outlook.com>
2023-03-21 15:45:03 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _sign -  prints the sign of a number
+ * print_sign -  prints the sign of a number
  * @n : the integer that will be checked
  * Return: 1 if n > zero, -1 if n <0 and 0 if n=0
  */
@@ -9,16 +9,16 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar(43);
-return 1;
+ return (1);
 }
 else if (n < 0)
 {
 _putchar(45);
-return -1;
+ return (-1);
 }
 else
 {
 _putchar(48);
-return 0;
+ return (0);
 }
 }
