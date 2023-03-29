0151b1e263b78ba2ee32be86e7fb66e489146d8e
raikling <raiklings@outlook.com>
2023-03-28 18:10:21 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 # include "main.h"
 /**
- * reset_to_98 updates the value it points to to 98
+ * reset_to_98 - updates the value it points to to 98
  * @n: integer input pointer
  * Return: void
  */
