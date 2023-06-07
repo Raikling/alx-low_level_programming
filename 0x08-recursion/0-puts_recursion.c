b7bbbb3209e1457c0f71a4faa8808f5e04549638
Raikling <raiklings@outlook.com>
2023-06-07 11:23:38 -0700

    fixed Betty style

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * _puts_recursion - function 
- * @s: input
- * Return: always 0
- */
+* _puts_recursion - function
+* @s: input
+* Return: always 0
+*/
 void _puts_recursion(char *s)
 {
-  if (*s)
-    {
-      _putchar(*s);
-      _puts_recursion(s + 1);
-    }
-  else
-    _putchar('\n');
+if (*s)
+{
+_putchar(*s);
+_puts_recursion(s + 1);
+}
+else
+_putchar('\n');
 }
