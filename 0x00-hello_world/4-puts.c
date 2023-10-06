5e2382951e88669f16437925f6ba633d9a134edb
raikling <raiklings@outlook.com>
2023-10-05 18:07:23 -0700

    fix1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main : program
- * @Return : always 0
- */
-int main (void)
+*main- entry point
+*return : always 0 (success)
+*/
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
