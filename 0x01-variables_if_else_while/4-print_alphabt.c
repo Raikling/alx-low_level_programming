156b31f248e46719165c719252ac87913aad1f61
raikling <raiklings@outlook.com>
2023-10-06 16:29:38 -0700

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets except q and e
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+char alpha[26] = "abcdefghijklmnopqrstuvwxyz";
+int i;
+for (i = 0; i < 26; i++)
+{
+if (alpha[i] == 'q' || alpha[i] == 'e')
+{
+continue;
+}
+putchar(alpha[i]);
+}
+putchar(alpha'\n');
+return (0);
+}
+
