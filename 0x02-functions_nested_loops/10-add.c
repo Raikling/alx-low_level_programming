a1b80b4728bb926903d90cdd17f1c6072bab4aa3
raikling <raiklings@outlook.com>
2023-10-10 16:59:21 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds 2 integers and returns result
+ * @num1 : number 1
+ * @num2: number 2
+ * Return: addition of 2 number
+ */
+int add(int num1, int num2)
+{
+return (num1 + num2);
+}
