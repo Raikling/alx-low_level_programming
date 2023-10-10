b535c1dfe28f61d3fbfc0994120ca5d9db783475
raikling <raiklings@outlook.com>
2023-10-10 14:33:17 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabets in lowercase
+ * Return: Always (0)
+ */
+void print_alphabet(void)
+{
+for (int alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
+return (0);
+}
