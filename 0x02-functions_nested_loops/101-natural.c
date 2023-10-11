0b6a32b8ac1d2ea7f28ce2fa9fddd9aefdc21f6a
raikling <raiklings@outlook.com>
2023-10-10 17:54:06 -0700

    fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-  int i, sum;
-  for (i = 0; i < 1024; i++)
-    {
-      if ((i % 3 == 0) || (i % 5 == 0))
-	sum += sum;
-    }
-  printf("%d\n", sum);
-  return (0);
+int i, sum;
+for (i = 0; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+sum += sum;
+}
+printf("%d\n", sum);
+return (0);
 }
