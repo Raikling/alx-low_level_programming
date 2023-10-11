51c092621b516da74a4ad782aa3d453dd10c3c8f
raikling <raiklings@outlook.com>
2023-10-10 17:51:19 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+ * main - program to sum the natural num divadable by 3 or 5
+ * Return: always (0)
+ */
+int main(void)
+{
+  int i, sum;
+  for (i = 0; i < 1024; i++)
+    {
+      if ((i % 3 == 0) || (i % 5 == 0))
+	sum += sum;
+    }
+  printf("%d\n", sum);
+  return (0);
+}
