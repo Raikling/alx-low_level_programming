255d7786d2009048c0789216a71659afc66c5de0
raikling <raiklings@outlook.com>
2023-10-12 16:30:53 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 line of nums from 0 to 14
+ * Return: nothing
+ */
+void more_numbers(void)
+{
+int i, j, num;
+for (i = 0; i <= 10; i++)
+{
+for (j = 0; j <= 14; i++)
+{
+num = j;
+if (i > 9)
+{
+_putchar(1 + 48);
+num = j % 10;
+}
+_putchar(num + 48);
+}
+_putchar('\n');
+}
+}
