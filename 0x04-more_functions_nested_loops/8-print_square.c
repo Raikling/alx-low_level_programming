1d4e42b0125a9a36c823293977e62a24baa78c48
raikling <raiklings@outlook.com>
2023-10-12 17:02:31 -0700

    task8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_square - print a square #
+ * @size : of square
+ */
+void print_square(int size)
+{
+int row, col;
+for (row = 1; row <= size; row++)
+{
+for (col = 1; col <= size; col++)
+_putchar('#');
+_putchar('\n');
+}
