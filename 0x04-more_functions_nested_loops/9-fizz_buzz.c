f917c252528bc6cd40aa35a5dfd87b541f21819b
raikling <raiklings@outlook.com>
2023-10-12 17:16:09 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * main - prints 1 -100 then new line with fizz or buzz
+ * Return: Always (0)
+ */
+int main(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && !(i % 5 == 0))
+printf("Fizz");
+else if (i % 5 == 0 && !(i % 3 == 0))
+printf("Buzz");
+else if (i % 3 == 0 && i % 5 == 0)
+printf("FizzBuzz");
+else
+printf("%d", num);
+
+if (num != 100)
+printf(" ");
+else
+printf("\n");
+}
+return (0);
+}
