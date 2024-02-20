65dc2c0bbbe88b33a1f03f1566e28dc848efca8e
Raikling <raiklings@outlook.com>
2024-02-19 21:14:23 -0800

    fix2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,13 +11,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i = 0;
 if (array && size && cmp)
-{
 while (i < size)
 {
 if (cmp(array[i]))
 return (i);
 i++;
-}
 }
 return (-1);
 }
