abb3c99247b87840de6884d3f4dce530dba2c74c
Raikling <raiklings@outlook.com>
2024-02-21 05:53:27 -0800

    fix

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -17,10 +17,10 @@ int cd;
 if (argc != 2)
 printf("Error\n"), exit(1);
 
-b = atoi(argv[1]);
-if (b < 0)
+cd= atoi(argv[1]);
+if (cd < 0)
 printf("Error\n"), exit(2);
-while (b--)
-printf("%02hhx%s", *p++, b ? " " : "\n");
+while (cd--)
+printf("%02hhx%s", *p++, cd ? " " : "\n");
 return (0);
 }
